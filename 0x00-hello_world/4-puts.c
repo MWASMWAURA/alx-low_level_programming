ebdddf7eb4bef1613d1f1b7163f7effc53ff8151
MWASMWAURA <jamesmwauramwas@gmail.com>
2023-02-16 10:54:47 -0800

    commit updates

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,13 @@
 /**
  * main - prints "Programming is like building a multilingual
  *  puzzle,followed by a newline."
- * Return 0.
+ * Return: Always 0.
  */
 
 int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
 
