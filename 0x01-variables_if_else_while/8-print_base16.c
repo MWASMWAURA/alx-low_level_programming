fdb380c2630a986312fddf83f3dd3612d134e977
MWASMWAURA <jamesmwauramwas@gmail.com>
2023-02-20 17:46:29 -0800

    commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-
 /**
 *main - Entry point
 *Return: Always 0 (Success)
 */
-
 int main(void)
 {
 	int d;
 	char low;
-		
-	for (d = 'o'; d <= '9';d++)
+
+	for (d = 'o'; d <= '9'; d++)
 	putchar (d);
-	
+
 	for (low = 'a'; low <= 'f'; low++)
 	putchar(low);
 	putchar('\n');
-		
+	
 	return (0);
 }
