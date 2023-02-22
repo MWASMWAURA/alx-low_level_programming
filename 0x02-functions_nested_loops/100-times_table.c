8403fa2b1d6eaab9476b2fe10db2d27c7b74fe39
MWASMWAURA <jamesmwauramwas@gmail.com>
2023-02-22 13:28:20 -0800

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
-*print_to_98 - Prints the times table of the input,
+*print_times_table - Prints the times table of the input,
 *starting with 0.
 *@n: The value of the times table to be printed.
 */
 void print_times_table(int n)
 {
 	int num, mult, prod;
-	
+
 	if (n >= 0 && n <= 15)
 	{
 	for (num = 0; num <= n; num++)
