60ff4b50724b42fc870dc3217b1a2d18ad8ee8ef
MWASMWAURA <jamesmwauramwas@gmail.com>
2023-02-22 07:04:13 -0800

    5-sign

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,7 +7,7 @@
 void jack_bauer(void)
 {
 	int a, b, c, d;
-	
+
 	for (a = 0; a <= 2; a++)
 	{
 	for (b = 0; b <= 9; b++)
@@ -16,14 +16,16 @@ void jack_bauer(void)
 	{
 	for (c = 0; c <= 5; c++)
 	{
-	for (d = 0; d <= 0; d++);
-	}
+	for (d = 0; d <= 9; d++);
+	{
 	_putchar(a + '0');
 	_putchar(b + '0');
 	_putchar(58);
 	_putchar(c + '0');
 	_putchar(d + '0');
 	_putchar('\n');
+	}
+	}
 	}
 	}
 	}
