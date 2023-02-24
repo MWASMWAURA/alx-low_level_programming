03077fae5b7a8e48734c32a6665f2f455beb133a
MWASMWAURA <jamesmwauramwas@gmail.com>
2023-02-24 02:15:07 -0800

    1-isdigit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*mul -multiples two integers
+*@a: first int
+*@b: second int
+*Return: == (a * b)
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_line - draws a straight line in the terminal
+*@n: int 
+*/
+void print_line(void)
+{
+	int i;
+	
+	for (i = 0; i < n; i++)
+	{
+	_putchar('_');
+	}
+	_putchar('\n');
+}
