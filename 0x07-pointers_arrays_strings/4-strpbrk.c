87f046e2da042101af48c5d71d9d2dd1ab28f97b
MWASMWAURA <jamesmwauramwas@gmail.com>
2023-03-07 12:34:06 -0800

    update

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,10 +12,10 @@
 char *_strpbrk(char *s, char *accept)
 {
 	int index;
-	
+
 	while (*s)
 	{
-		for (index = 0;accept[index]; index++)
+		for (index = 0; accept[index]; index++)
 		{
 			if (*s == accept[index])
 				return (s);
